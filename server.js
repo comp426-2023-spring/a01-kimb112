2edccfcec5ff317f063fc535932c6543c1b4e693
Kimberly Brown <kbrown1@unc.edu>
2023-03-31 10:45:39 -0400

    server

--- server.js
+++ server.js
@@ -30,14 +30,14 @@ if (err) {
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', `text/html`);
     res.end(data);
 })
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
-    console.log("Server listening on port ${port}");
+    console.log(`Server listening on port ${port}`);
 });
 // That's it! You're all done!
 });
